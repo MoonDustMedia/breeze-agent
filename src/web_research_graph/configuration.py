2f393bf9c6be53b249eecd8737da75a368c9f795
Simon Paredes <paredess@gmail.com>
2025-02-28 18:33:07 -0300

    lets use just 4 results

--- src/web_research_graph/configuration.py
+++ src/web_research_graph/configuration.py
@@ -49,7 +49,7 @@ class Configuration:
     
 
     max_search_results: int = field(
-        default=10,
+        default=4,
         metadata={
             "description": "The maximum number of search results to return for each search query."
         },
